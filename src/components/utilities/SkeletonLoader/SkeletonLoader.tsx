2771ea7093c370f00cd6d46bf4bf397805961b0a
triantafd <dimitris12313@hotmail.com>
2023-09-08 12:32:15 +0300

    fix skeletonnot having unique key

--- src/components/utilities/SkeletonLoader/SkeletonLoader.tsx
+++ src/components/utilities/SkeletonLoader/SkeletonLoader.tsx
@@ -10,7 +10,7 @@ const SkeletonLoader: React.FC<ISkeletonLoaderrProps> = ({ pageSize }) => {
   return (
     <tbody className="skeleton-wrapper">
       {[...Array(pageSize)].map((item, i) =>
-        <tr>
+        <tr key={`skeleton-id-${i}`}>
           <td className="skeleton-row"></td>
           <td className="skeleton-row"></td>
           <td className="skeleton-row"></td>
